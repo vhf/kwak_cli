d153131cefcf9aa8ea21333303bd4fc061a50941
Victor Felder <victorfelder@gmail.com>
2015-06-17 17:32:30 +0200

    initial commit

---
+++ client.py
@@ -0,0 +1,47 @@
+from pprint import pprint
+from time import sleep
+
+from MeteorClient import MeteorClient
+
+
+class Client:
+    def __init__(self, username, password, ui):
+        self.ui = ui
+        self.client = MeteorClient('ws://127.0.0.1:3000/websocket')
+        self.client.connect()
+        self.client.login(user=username, password=password)
+
+        self.client.on('connected', self.connected)
+
+        self.hot_channels = []
+        self.hot_channels_name = []
+        self.client.call('getHotChannels', [], self.set_hot_channels_name)
+
+        self.client.on('added', self.added)
+
+    def set_hot_channels_name(self, error, result):
+        if error:
+            self.ui.chatbuffer_add(error)
+            return
+        self.hot_channels_name = result
+
+    def subscribe_to_channel(self, channel='main'):
+        try:
+            self.client.unsubscribe('messages')
+        except:
+            pass
+        self.ui.chatbuffer_add('* LISTENING TO CHANNEL {}'.format(channel))
+        self.client.subscribe('messages', [channel])
+
+    def subscribe_to_users(self):
+        self.client.subscribe('users')
+
+    def added(self, collection, id, fields):
+        if collection == 'messages':
+            self.ui.chatbuffer_add(fields['text'])
+        elif collection == 'users':
+            self.ui.userlist.append(fields['username'])
+            self.ui.redraw_userlist()
+
+    def connected(self):
+        self.ui.chatbuffer_add('* CONNECTED')
---
+++ main.py
@@ -0,0 +1,27 @@
+from curses import wrapper
+from ui import ChatUI
+from client import Client
+import ConfigParser
+
+def main(stdscr):
+    cp = ConfigParser.ConfigParser()
+    cp.read('config.cfg')
+    username = cp.get('credentials', 'username')
+    password = cp.get('credentials', 'password')
+
+    stdscr.clear()
+    ui = ChatUI(stdscr)
+
+    client = Client(username, password, ui)
+    client.subscribe_to_channel('main')
+    client.subscribe_to_users()
+
+    message = ''
+    while message != '/quit':
+        message = ui.wait_input()
+        if message[0:6] == '/join ':
+            client.subscribe_to_channel(message[6:])
+        else:
+            ui.chatbuffer_add(message)
+
+wrapper(main)
