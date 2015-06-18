c38a8e209e69bae7a8c5e49f22f74afd56c2bd5a
Victor Felder <victorfelder@gmail.com>
2015-06-18 14:00:14 +0200

    Login system and config

--- client.py
+++ client.py
@@ -6,18 +6,20 @@ from MeteorClient import MeteorClient
 
 class Client:
     def __init__(self, username, password, ui):
+        self.username = username
+        self.password = password
         self.ui = ui
         self.client = MeteorClient('ws://127.0.0.1:3000/websocket')
         self.client.connect()
-        self.client.login(user=username, password=password)
+        self.client.login(self.username, self.password, callback=self.logged_in)
 
-        self.client.on('connected', self.connected)
 
         self.hot_channels = []
         self.hot_channels_name = []
         self.current_channel = 'main'
         self.client.call('getHotChannels', [], self.set_hot_channels_name)
 
+        self.client.on('connected', self.connected)
         self.client.on('added', self.added)
 
     def set_hot_channels_name(self, error, result):
@@ -40,10 +42,17 @@ class Client:
 
     def added(self, collection, id, fields):
         if collection == 'messages':
-            self.ui.chatbuffer_add(fields['text'])
+            pass
+            # self.ui.chatbuffer_add(fields['text'])
         elif collection == 'users':
             self.ui.userlist.append(fields['username'])
             self.ui.redraw_userlist()
 
     def connected(self):
         self.ui.chatbuffer_add('* CONNECTED')
+
+    def logged_in(self, error, data):
+        if error:
+            self.ui.chatbuffer_add('LOGIN ERROR {}'.format(error))
+        else:
+            self.ui.chatbuffer_add('* LOGGED IN')
