9db875e734e5fb9696bf5a6d769b154824d4c6cb
Victor Felder <victorfelder@gmail.com>
2015-06-26 14:14:06 +0200

    Refactor commands

---
+++ commands.py
@@ -0,0 +1,27 @@
+def dbg(ui, client, rest):
+    ui.redraw_userlist()
+    ui.chatbuffer_add(str(client.ui.userlist))
+    client.client.call('setOnline', [])
+
+def hot(ui, client, rest):
+    ui.chatbuffer_add(', '.join(client.hot_channels_name))
+    client.client.call('getHotChannels', [], client.set_hot_channels_name)
+
+def join(ui, client, rest):
+    client.subscribe_to_channel(rest)
+
+def lst(ui, client, rest):
+    ui.chatbuffer_add(', '.join(client.all_channels_name));
+    client.client.call('channelList', [], client.set_all_channels_name)
+    ui.redraw_ui()
+
+def quit(ui, client, rest):
+    pass
+
+commands = {
+    'dbg':  [dbg, 'SYNOPSYS: lala', 'USAGE: lala'],
+    'hot':  [hot, 'SYNOPSYS: lala', 'USAGE: lala'],
+    'join': [join, 'SYNOPSYS: lala', 'USAGE: lala'],
+    'list': [lst, 'SYNOPSYS: lala', 'USAGE: lala'],
+    'quit': [quit, 'SYNOPSYS: lala', 'USAGE: lala'],
+}
