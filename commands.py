463cd201645ff5b02beb3c085638a95daa08a78b
Robin Guinant <robin.guinant@gmail.com>
2015-07-01 14:11:04 +0200

    /invite added

--- commands.py
+++ commands.py
@@ -8,6 +8,15 @@ def hot(ui, client, rest):
     ui.chatbuffer_add(', '.join(client.hot_channels_name))
     client.client.call('getHotChannels', [], client.set_hot_channels_name)
 
+def invt(ui, client, rest):
+    if (rest == None):
+        return 1
+    rest = rest.split(' ', 1)
+    rest[1] = rest[1].replace(' ', "")
+    if (len(rest) < 1):
+        return 1
+    client.client.call('invite', rest)
+    
 def join(ui, client, rest):
     client.subscribe_to_channel(rest)
 
@@ -20,10 +29,11 @@ def quit(ui, client, rest):
     exit(0)
 
 commands = {
-    'dbg':  [dbg, 'SYNOPSYS: lala', 'USAGE: lala'],
-    'hot':  [hot, 'SYNOPSYS: lala', 'USAGE: lala'],
-    'j':    [join, 'SYNOPSYS: lala', 'USAGE: lala'],
-    'join': [join, 'SYNOPSYS: lala', 'USAGE: lala'],
-    'list': [lst, 'SYNOPSYS: lala', 'USAGE: lala'],
-    'quit': [quit, 'SYNOPSYS: lala', 'USAGE: lala'],
+    'dbg':      [dbg, 'SYNOPSYS: lala', 'USAGE: lala'],
+    'hot':      [hot, 'SYNOPSYS: lala', 'USAGE: lala'],
+    'invite':   [invt, 'SYNOPSYS: lala', 'USAGE: lala'],
+    'j':        [join, 'SYNOPSYS: lala', 'USAGE: lala'],
+    'join':     [join, 'SYNOPSYS: lala', 'USAGE: lala'],
+    'list':     [lst, 'SYNOPSYS: lala', 'USAGE: lala'],
+    'quit':     [quit, 'SYNOPSYS: lala', 'USAGE: lala'],
 }
